f493839c49a02ae526baeabf83173b600d18a239
Yurii BUHAI <ybuhai@e1r9p9.unit.ua>
2019-10-30 21:42:17 +0200

    setka

--- src/write_data_to_window.c
+++ src/write_data_to_window.c
@@ -6,7 +6,7 @@
 /*   By: ybuhai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 20:03:33 by ybuhai            #+#    #+#             */
-/*   Updated: 2019/10/29 21:12:39 by ybuhai           ###   ########.fr       */
+/*   Updated: 2019/10/30 20:35:06 by ybuhai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 
 void	write_data_to_window(t_fdf *fdf)
 {
+	for(int x = 0; x < fdf->width; x++)
+		for (int y = 0; y < (fdf->heigth - 1) * SCALE; y++)
+			fdf->data[WINDOW_SIZE_X * y + x * SCALE] = 0xff;
 	for (int j = 0; j < fdf->heigth; j++)
-		for (int i = 0; i < fdf->width * SCALE; i++)
-		{
-			fdf->data[i + (j * WINDOW_SIZE_X * SCALE)] = 0x000ff;
+		for (int i = 0; i <= (fdf->width - 1) * SCALE; i++)
 			if (i % SCALE == 0) 
-			fdf->data[i + (j * WINDOW_SIZE_X * SCALE)] = 0xff0000;
-		}/*	
-	for (int j = 0; j < fdf->heigth * SCALE; j++)
-		for (int i = 0; i < fdf->width; i++)
-		{
-			fdf->data[j + (i * WINDOW_SIZE_X
-		}*/
+				fdf->data[i + (j * WINDOW_SIZE_X * SCALE)] = 0xff0000;
+			else
+				fdf->data[i + (j * WINDOW_SIZE_X * SCALE)] = 0xff;
 	mlx_put_image_to_window(fdf->mlx, fdf->window, fdf->img, SCALE, SCALE);
 }
